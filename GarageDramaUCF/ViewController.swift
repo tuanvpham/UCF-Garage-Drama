cd08a21fd69c87a1ae2857026476edfaa8b8b360
Tuan Pham <tuanvpham32@outlook.com>
2018-02-20 12:36:15 -0500

    Initial Commit

---
+++ GarageDramaUCF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GarageDramaUCF
+//
+//  Created by Tuan Pham on 2/20/18.
+//  Copyright © 2018 Tuan Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
